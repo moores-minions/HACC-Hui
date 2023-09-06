39fc917775dcae086bffd7ed294b82960cc988bb
Zolbo Tomita <2020TomitaZ@gmail.com>
2023-09-05 21:58:31 -1000

    Update NotFound.jsx

--- app/imports/ui/pages/NotFound.jsx
+++ app/imports/ui/pages/NotFound.jsx
@@ -5,14 +5,12 @@ import { Container } from 'react-bootstrap';
  * Render a Not Found page if the user enters a URL that doesn't match any route.
  * @memberOf ui/pages
  */
-class NotFound extends React.Component {
-  render() {
-    return (
-      <Container fluid>
-        <h2 className="text-center">Page not found</h2>
-      </Container>
-    );
-  }
-}
+const NotFound = () => {
+  return (
+    <Container id='notfound-page' fluid>
+      <h2 className="text-center">Page not found</h2>
+    </Container>
+  );
+};
 
 export default NotFound;
