9a499c7f55a24a0b75dd8bdc310e6ad4593fa0eb
Jayden Tactay <75406323+jaydent22@users.noreply.github.com>
2023-09-08 09:39:47 -1000

    AddTool.jsx test

--- app/tests/tests.testcafe.js
+++ app/tests/tests.testcafe.js
@@ -1,4 +1,5 @@
 import { landingPage } from './landing.page';
+import { addtoolPage } from './addtool.page';
 
 /* global fixture:false, test:false */
 
@@ -6,9 +7,12 @@ import { landingPage } from './landing.page';
 // const credentials = { username: 'admin@foo.com', password: 'changeme' };
 // const newCreds = { username: 'abc@foo.com', licensePlate: 'ABC123', password: 'changeme', hasPass: 'True' };
 
+const testaddtool = { name: 'test', description: 'testing' };
+
 fixture('HACC-HUI Test')
   .page('http://localhost:3400');
 
 test('Test that landing page shows up and all pages work', async (testController) => {
   await landingPage.isDisplayed(testController);
+  await addtoolPage.addSkill(testController, testaddtool.name, testaddtool.description);
 });
