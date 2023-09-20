29ed64929703d2e2b98e772a8499b790207dca96
Hooobot <hoobot0@gmail.com>
2023-09-20 09:10:49 -1000

    fix deplot -- fix filepath for import

--- app/imports/ui/pages/HelpPage.jsx
+++ app/imports/ui/pages/HelpPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-import './ui.css'; // Import the CSS file
+import '../ui.css'; // Import the CSS file
 
 const HelpPage = () => (
       <Container id="help-page" fluid className="help-page-container">
