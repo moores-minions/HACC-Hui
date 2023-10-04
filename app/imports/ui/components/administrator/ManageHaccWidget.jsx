03f6b58aa8e9ded94f7c8240da32574b0b8d79de
chenglisa <97767296+chenglisa@users.noreply.github.com>
2023-10-04 03:17:53 -1000

    convert bootstrap

--- app/imports/ui/components/administrator/ManageHaccWidget.jsx
+++ app/imports/ui/components/administrator/ManageHaccWidget.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import { Grid, Segment, Header, Radio } from 'semantic-ui-react';
-import { Table, Button } from 'react-bootstrap';
+import { Table, Button, Container, Row, Col, Form } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { withTracker } from 'meteor/react-meteor-data';
@@ -60,29 +59,39 @@ class ManageHaccWidget extends React.Component {
     // console.log(this.state);
     return (
         <div id="configureHACCPage" style={{ paddingBottom: '50px' }}>
-          <Grid container centered>
-            <Grid.Column>
-              <div style={{
-                backgroundColor: '#E5F0FE', padding: '1rem 0rem', margin: '2rem 0rem',
-                borderRadius: '2rem',
-              }}>
-                <h3 className="text-center">Manage HACC</h3>
-                <Header as="h5" textAlign="center">
-                  <Radio toggle label="Can Create Teams"
-                          checked={this.state.canCreateTeams}
-                          onChange={this.toggleTeam} />&nbsp;
-                  <Radio toggle label="Can Change Challenges"
-                         checked={this.state.canChangeChallenges}
-                         onChange={this.toggleChallenge} />
-                </Header>
-              </div>
-              <Segment style={{
-                borderRadius: '1rem',
-                backgroundColor: '#E5F0FE',
-              }} className={'teamCreate'}>
-                <h3 className="text-center">Challenges</h3>
-                <Table fixed columns={5}>
-                  <thead>
+          <Container>
+            <Row className="justify-content-center">
+              <Col>
+                <div style={{
+                  backgroundColor: '#E5F0FE', padding: '1rem 0rem', margin: '2rem 0rem',
+                  borderRadius: '2rem',
+                }}>
+                  <h3 className="text-center">Manage HACC</h3>
+                  <Form.Group className="text-center justify-content-center">
+                    <Form.Check className="justify-content-center"
+                      type="switch"
+                      id="custom-switch"
+                      label="Can Create Teams"
+                      checked={this.state.canCreateTeams}
+                      onChange={this.toggleTeam}
+                    />
+                    <Form.Check
+                      type="switch"
+                      id="custom-switch"
+                      label="Can Change Challenges"
+                      checked={this.state.canChangeChallengs}
+                      onChange={this.toggleChallenge}
+                    />
+                  </Form.Group>
+                </div>
+                <div style={{
+                  borderRadius: '1rem',
+                  backgroundColor: '#E5F0FE',
+                }} className={'teamCreate'}>
+
+                  <h3 className="text-center">Challenges</h3>
+                  <Table>
+                    <thead>
                     <tr>
                       <th width={2}>Title</th>
                       <th width={5}>Description</th>
@@ -91,62 +100,59 @@ class ManageHaccWidget extends React.Component {
                       <th width={2}>Edit</th>
                       <th width={2}>Delete</th>
                     </tr>
-                  </thead>
-                  {/* eslint-disable-next-line max-len */}
-                  <tbody>{this.props.challenges.map((challenges => <ChallengesAdminWidget key={challenges._id} challenges={challenges} />
-                  ))}
-                  </tbody>
-                </Table>
-                <div align='center'>
-                  <Button id="add-challenge-button" variant="danger">
-                    <Link to={ROUTES.ADD_CHALLENGE}>Add Challenge</Link></Button>
-                  <Button id='add-challenge-button' style={{
-                    color: 'white', backgroundColor: '#DB2828',
-                    margin: '2rem 0rem',
-                  }}><Link to={ROUTES.ADD_CHALLENGE} style={{ color: 'white' }}>Add Challenge</Link></Button>
-                </div>
-                <h3 className="text-center">Skills</h3>
-                <Table>
-                  <thead>
+                    </thead>
+                    <tbody>
+                    {this.props.challenges.map((challenges => <ChallengesAdminWidget
+                        key={challenges._id} challenges={challenges} />
+                    ))}
+                    </tbody>
+                  </Table>
+                  <div className="text-center">
+                    <Button className="hacc-button" id="add-challenge-button" variant="danger">
+                      <Link to={ROUTES.ADD_CHALLENGE} className="white-link">Add Challenge</Link></Button>
+                  </div>
+
+                  <h3 className="text-center">Skills</h3>
+                  <Table>
+                    <thead>
                     <tr>
                       <th>Name</th>
                       <th>Description</th>
                       <th width={2}>Edit</th>
                       <th width={2}>Delete</th>
                     </tr>
-                  </thead>
-                  {/* eslint-disable-next-line max-len */}
-                  <tbody>{this.props.skills.map((skills => <SkillsAdminWidget key={skills._id} skills={skills} />))}
-                  </tbody>
-                </Table>
-                <div align='center'>
-                  <Button id="add-skill-button" style={{
-                    color: 'white', backgroundColor: '#DB2828',
-                    margin: '2rem 0rem',
-                  }}><Link to={ROUTES.ADD_SKILL} style={{ color: 'white' }}>Add Skill</Link></Button>
-                </div>
-                <h3 className="text-center">Tools</h3>
-                <Table>
-                  <thead>
+                    </thead>
+                    <tbody>
+                    {this.props.skills.map((skills => <SkillsAdminWidget key={skills._id} skills={skills} />))}
+                    </tbody>
+                  </Table>
+                  <div className="text-center">
+                    <Button className="hacc-button" id="add-skill-button" variant="danger">
+                      <Link to={ROUTES.ADD_SKILL} className="white-link">Add Skill</Link></Button>
+                  </div>
+
+                  <h3 className="text-center">Tools</h3>
+                  <Table>
+                    <thead>
                     <tr>
                       <th>Name</th>
                       <th>Description</th>
                       <th width={2}>Edit</th>
                       <th width={2}>Delete</th>
                     </tr>
-                  </thead>
-                  <tbody>{this.props.tools.map((tools => <ToolsAdminWidget key={tools._id} tools={tools} />))}
-                  </tbody>
-                </Table>
-                <div align='center'>
-                  <Button id='add-tool-button' style={{
-                    color: 'white', backgroundColor: '#DB2828',
-                    margin: '2rem 0rem',
-                  }}><Link to={ROUTES.ADD_TOOL} style={{ color: 'white' }}>Add Tool</Link></Button>
+                    </thead>
+                    <tbody>
+                    {this.props.tools.map((tools => <ToolsAdminWidget key={tools._id} tools={tools} />))}
+                    </tbody>
+                  </Table>
+                  <div className="text-center">
+                    <Button className="hacc-button" id="add-tool-button" variant="danger">
+                      <Link to={ROUTES.ADD_TOOL} className="white-link">Add Tool</Link></Button>
+                  </div>
                 </div>
-              </Segment>
-            </Grid.Column>
-          </Grid>
+              </Col>
+            </Row>
+          </Container>
         </div>
     );
   }
