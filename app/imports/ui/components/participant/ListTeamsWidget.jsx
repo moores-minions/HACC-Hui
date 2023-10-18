66c72c1359a4f7b2044b1411edd4e38e1d2e5fb7
chenglisa <97767296+chenglisa@users.noreply.github.com>
2023-10-17 21:41:57 -1000

    convert to bootstrap

--- app/imports/ui/components/participant/ListTeamsWidget.jsx
+++ app/imports/ui/components/participant/ListTeamsWidget.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Grid, Header, Message } from 'semantic-ui-react';
+import { Container, Row, Col, Alert } from 'react-bootstrap';
+// import { Grid, Header, Message } from 'semantic-ui-react';
 import ListTeamExampleWidget from './ListTeamExampleWidget';
 import { TeamChallenges } from '../../../api/team/TeamChallengeCollection';
 import { Challenges } from '../../../api/challenge/ChallengeCollection';
@@ -47,41 +48,85 @@ class ListTeamsWidget extends React.Component {
   render() {
     const closed = Teams.find({ open: false }).count();
     return (
-        <Grid celled doubling stackable>
-          <Grid.Row columns={7} only='computer'>
-            <Grid.Column>
-              <Header>Name</Header>
-            </Grid.Column>
-            <Grid.Column>
-              <Header>Challenges</Header>
-            </Grid.Column>
-            <Grid.Column>
-              <Header>Desired Skills</Header>
-            </Grid.Column>
-            <Grid.Column>
-              <Header>Desired Tools</Header>
-            </Grid.Column>
-            <Grid.Column>
-              <Header>Devpost / Github</Header>
-            </Grid.Column>
-            <Grid.Column>
-              <Header>Members</Header>
-            </Grid.Column>
-            <Grid.Column>
-              <Header>Join?</Header>
-            </Grid.Column>
-          </Grid.Row>
-          {this.props.teams.map((team) => (
-              <ListTeamExampleWidget key={team._id}
-                                     team={getTeam(team._id)}
-                                     teamChallenges={getTeamChallenges(team)}
-                                     teamSkills={getTeamSkills(team)}
-                                     teamTools={getTeamTools(team)}
-                                     teamMembers={getTeamMembers(team)}
-              />
-          ))}
-          <Grid.Row><Message negative>There are {closed} closed teams.</Message></Grid.Row>
-        </Grid>
+      <Container fluid>
+        <Row className="d-none d-md-flex">
+          <Col className="border">
+            <h4>Name</h4>
+          </Col>
+          <Col className="border">
+            <h4>Challenges</h4>
+          </Col>
+          <Col className="border">
+            <h4>Desired Skills</h4>
+          </Col>
+          <Col className="border">
+            <h4>Desired Tools</h4>
+          </Col>
+          <Col className="border">
+            <h4>Devpost / Github</h4>
+          </Col>
+          <Col className="border">
+            <h4>Members</h4>
+          </Col>
+          <Col className="border">
+            <h4>Join?</h4>
+          </Col>
+        </Row>
+
+        {this.props.teams.map((team) => (
+                <ListTeamExampleWidget key={team._id}
+                                       team={getTeam(team._id)}
+                                       teamChallenges={getTeamChallenges(team)}
+                                       teamSkills={getTeamSkills(team)}
+                                       teamTools={getTeamTools(team)}
+                                       teamMembers={getTeamMembers(team)}
+                />
+            ))}
+
+        <Row>
+          <Col>
+            <Alert variant="danger">
+              There are {closed} closed teams.
+            </Alert>
+          </Col>
+        </Row>
+      </Container>
+
+        // <Grid>
+        //   <Grid.Row columns={7} only='computer'>
+        //     <Grid.Column>
+        //       <Header>Name</Header>
+        //     </Grid.Column>
+        //     <Grid.Column>
+        //       <Header>Challenges</Header>
+        //     </Grid.Column>
+        //     <Grid.Column>
+        //       <Header>Desired Skills</Header>
+        //     </Grid.Column>
+        //     <Grid.Column>
+        //       <Header>Desired Tools</Header>
+        //     </Grid.Column>
+        //     <Grid.Column>
+        //       <Header>Devpost / Github</Header>
+        //     </Grid.Column>
+        //     <Grid.Column>
+        //       <Header>Members</Header>
+        //     </Grid.Column>
+        //     <Grid.Column>
+        //       <Header>Join?</Header>
+        //     </Grid.Column>
+        //   </Grid.Row>
+        //   {this.props.teams.map((team) => (
+        //       <ListTeamExampleWidget key={team._id}
+        //                              team={getTeam(team._id)}
+        //                              teamChallenges={getTeamChallenges(team)}
+        //                              teamSkills={getTeamSkills(team)}
+        //                              teamTools={getTeamTools(team)}
+        //                              teamMembers={getTeamMembers(team)}
+        //       />
+        //   ))}
+        //   <Grid.Row><Message negative>There are {closed} closed teams.</Message></Grid.Row>
+        // </Grid>
     );
   }
 }
