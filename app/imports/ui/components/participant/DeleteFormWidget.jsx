5ad28f6ee6aec346d17229e9f9b63d1a53cc6458
Hooobot <hoobot0@gmail.com>
2023-10-17 21:57:00 -1000

    Bootstrap conversion for DeleteFormWidget.jsx

--- app/imports/ui/components/participant/DeleteFormWidget.jsx
+++ app/imports/ui/components/participant/DeleteFormWidget.jsx
@@ -2,9 +2,13 @@ import React from 'react';
 import _ from 'lodash';
 import { withRouter } from 'react-router';
 import { Meteor } from 'meteor/meteor';
-import { Grid, Segment, Header } from 'semantic-ui-react';
-import { AutoForm, ErrorsField, LongTextField, SelectField } from 'uniforms-semantic';
-import Button from 'semantic-ui-react/dist/commonjs/elements/Button';
+import { Container, Row, Col, Button, Form } from 'react-bootstrap';
+import {
+  AutoForm,
+  ErrorsField,
+  LongTextField,
+  SelectField,
+} from 'uniforms-semantic';
 import { SimpleSchema2Bridge } from 'uniforms-bridge-simple-schema-2';
 import SimpleSchema from 'simpl-schema';
 import { withTracker } from 'meteor/react-meteor-data';
@@ -12,13 +16,15 @@ import PropTypes from 'prop-types';
 import swal from 'sweetalert';
 import { Participants } from '../../../api/user/ParticipantCollection';
 import { removeItMethod } from '../../../api/base/BaseCollection.methods';
-import { deleteAccountMethod, userInteractionDefineMethod } from '../../../api/user/UserInteractionCollection.methods';
+import {
+  deleteAccountMethod,
+  userInteractionDefineMethod,
+} from '../../../api/user/UserInteractionCollection.methods';
 import { USER_INTERACTIONS } from '../../../startup/client/user-interaction-constants';
 import { Teams } from '../../../api/team/TeamCollection';
 import { TeamParticipants } from '../../../api/team/TeamParticipantCollection';
 
 class DeleteFormWidget extends React.Component {
-
   submit(data) {
     const username = this.props.participant.username;
     const type = USER_INTERACTIONS.DELETE_ACCOUNT;
@@ -28,14 +34,14 @@ class DeleteFormWidget extends React.Component {
       type,
       typeData,
     };
-    userInteractionDefineMethod.call(userInteraction, (error) => (error ?
-            swal('Error', error.message, 'error') :
-            swal('Account deleted', 'We hope to see you again!', 'success')
-                .then(() => {
-                  // eslint-disable-next-line no-undef
-                  window.location = '/';
-                })
-    ));
+    userInteractionDefineMethod.call(userInteraction, (error) => (error
+        ? swal('Error', error.message, 'error')
+        : swal('Account deleted', 'We hope to see you again!', 'success').then(
+            () => {
+              // eslint-disable-next-line no-undef
+              window.location = '/';
+            },
+          )));
     const selector = { owner: this.props.participant._id };
     const ownedTeams = Teams.find(selector).fetch();
     _.forEach(ownedTeams, (team) => {
@@ -76,48 +82,54 @@ class DeleteFormWidget extends React.Component {
       other: { type: String, required: false },
     });
     const formSchema = new SimpleSchema2Bridge(schema);
+
     return (
-        <Grid container centered>
-          <Grid.Column>
-            <Header as="h2" textAlign="center">Feedback</Header>
-            <AutoForm schema={formSchema} onSubmit={data => {
-              swal({
-                text: 'Are you sure you want to delete your account?',
-                icon: 'warning',
-                buttons: true,
-                dangerMode: true,
-              })
-                  .then((willDelete) => {
-                    if (willDelete) {
-                      this.submit(data);
-                    } else {
-                      swal('Canceled deleting your account');
-                    }
-                  });
-            }}>
-              <Segment>
-                <Header as="h3">We&apos;re sorry to hear you&apos;re deleting your account.</Header>
-                <Header as="h4">Please provide feedback on why you&apos;re leaving
-                  to improve the HACC experience for next year.</Header>
-                <br/>
-                <Grid>
-                  <Grid.Row columns={2}>
-                    <Grid.Column>
-                      <SelectField name='feedback'/>
-                    </Grid.Column>
-                    <Grid.Column>
-                      <LongTextField name='other'/>
-                    </Grid.Column>
-                  </Grid.Row>
-                </Grid>
-                <Button basic color='red' value='submit'>
+      <Container>
+        <Row className="justify-content-md-center">
+          <Col md="auto">
+            <h2 className="text-center">Feedback</h2>
+            <AutoForm
+              schema={formSchema}
+              onSubmit={data => {
+                swal({
+                  text: 'Are you sure you want to delete your account?',
+                  icon: 'warning',
+                  buttons: true,
+                  dangerMode: true,
+                })
+                    .then((willDelete) => {
+                      if (willDelete) {
+                        this.submit(data);
+                      } else {
+                        swal('Canceled deleting your account');
+                      }
+                    });
+              }}>
+              <Form.Group>
+                <h3>
+                  We&apos;re sorry to hear you&apos;re deleting your account.
+                </h3>
+                <h4>
+                  Please provide fee dback on why you&apos;re leaving to improve
+                  the HACC experience for next year.
+                </h4>
+                <Row>
+                  <Col>
+                    <SelectField name="feedback" />
+                  </Col>
+                  <Col>
+                    <LongTextField name="other" />
+                  </Col>
+                </Row>
+                <Button variant="outline-danger" type="submit">
                   Delete Account
                 </Button>
-                <ErrorsField/>
-              </Segment>
+                <ErrorsField />
+              </Form.Group>
             </AutoForm>
-          </Grid.Column>
-        </Grid>
+          </Col>
+        </Row>
+      </Container>
     );
   }
 }
