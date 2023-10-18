cf0ef7f8658ad64df0b856f80ad77f8376dce23b
chenglisa <97767296+chenglisa@users.noreply.github.com>
2023-10-18 03:42:50 -1000

    convert bootstrap

--- app/imports/ui/components/participant/SuggestToolSkillWidget.jsx
+++ app/imports/ui/components/participant/SuggestToolSkillWidget.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import { Header, Segment, Form, Container, Card } from 'semantic-ui-react';
+import { Card, Form, Container } from 'react-bootstrap';
+import { AutoForm, SelectField, SubmitField, TextField } from 'uniforms-bootstrap5';
+// import { Header, Segment, Form, Container, Card } from 'semantic-ui-react';
 import { withTracker } from 'meteor/react-meteor-data';
-import {
-  AutoForm,
-  SelectField,
-  SubmitField,
-  TextField,
-} from 'uniforms-semantic';
+// import {
+//   AutoForm,
+//   SelectField,
+//   SubmitField,
+//   TextField,
+// } from 'uniforms-semantic';
 import { Meteor } from 'meteor/meteor';
 import PropTypes from 'prop-types';
 import { SimpleSchema2Bridge } from 'uniforms-bridge-simple-schema-2';
@@ -17,20 +19,12 @@ import { defineMethod } from '../../../api/base/BaseCollection.methods';
 import { Suggestions } from '../../../api/suggestions/SuggestionCollection';
 import { paleBlueStyle } from '../../styles';
 
+const schema = new SimpleSchema({
+  type: { type: String, allowedValues: ['Tool', 'Skill'], optional: false },
+  name: String,
+  description: String,
+});
 class SuggestToolSkillWidget extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { redirectToReferer: false };
-  }
-
-  buildTheFormSchema() {
-    const schema = new SimpleSchema({
-      type: { type: String, allowedValues: ['Tool', 'Skill'], optional: false },
-      name: String,
-      description: String,
-    });
-    return schema;
-  }
 
   submit(data, formRef) {
     // console.log('CreateProfileWidget.submit', data);
@@ -41,7 +35,6 @@ class SuggestToolSkillWidget extends React.Component {
     newData.name = data.name;
     newData.type = data.type;
     newData.description = data.description;
-    console.log(newData);
 
     defineMethod.call({ collectionName: collectionName, definitionData: newData },
         (error) => {
@@ -57,33 +50,54 @@ class SuggestToolSkillWidget extends React.Component {
   render() {
     let fRef = null;
     const model = this.props.participant;
-    const schema = this.buildTheFormSchema();
     const formSchema = new SimpleSchema2Bridge(schema);
     const firstname = model.firstName;
     return (
-        <Container style={{ paddingBottom: '50px', paddingTop: '40px' }}>
-        <Segment style = { paleBlueStyle }>
-          {/* eslint-disable-next-line max-len */}
-          <Header as="h2" textAlign="center">Hello {firstname}, please fill out the form to
-            suggest a new tool or skill. </Header>
-          <Card fluid>
-          <AutoForm ref={ref => {
-            fRef = ref;
-          }} schema={formSchema} onSubmit={data => this.submit(data, fRef)}>
-            <Form.Group widths="equal" style={{ paddingRight: '10px', paddingLeft: '10px',
-              paddingTop: '10px', paddingBottom: '10px' }}>
-              <SelectField name="type" />
-              <TextField name="name" />
-              <TextField name="description" />
-            </Form.Group>
-            <SubmitField style={{
-  display: 'block',
-  marginLeft: 'auto', marginRight: 'auto', marginBottom: '10px',
-}}/>
-          </AutoForm>
+      <Container style={{ paddingBottom: '50px', paddingTop: '40px' }}>
+        <Card style = { paleBlueStyle }>
+            <h2 className="text-center">Hello {firstname}, please fill out the form
+              to suggest a new tool or skill.</h2>
+          <Card>
+            <AutoForm ref={ref => {
+              fRef = ref;
+            }} schema={formSchema} onSubmit={data => this.submit(data, fRef)}>
+              <Form.Group widths="equal" style={{ paddingRight: '10px', paddingLeft: '10px',
+                paddingTop: '10px', paddingBottom: '10px' }}>
+                    <SelectField name="type" />
+                    <TextField name="name" />
+                    <TextField name="description" />
+                    <SubmitField style={{
+                      display: 'block',
+                      marginLeft: 'auto', marginRight: 'auto', marginBottom: '10px',
+                    }} value='Submit'/>
+              </Form.Group>
+            </AutoForm>
           </Card>
-        </Segment>
-        </Container>
+        </Card>
+      </Container>
+//         <Container style={{ paddingBottom: '50px', paddingTop: '40px' }}>
+//         <Segment style = { paleBlueStyle }>
+//           {/* eslint-disable-next-line max-len */}
+//           <Header as="h2" textAlign="center">Hello {firstname}, please fill out the form to
+//             suggest a new tool or skill. </Header>
+//           <Card fluid>
+//           <AutoForm ref={ref => {
+//             fRef = ref;
+//           }} schema={formSchema} onSubmit={data => this.submit(data, fRef)}>
+//             <Form.Group widths="equal" style={{ paddingRight: '10px', paddingLeft: '10px',
+//               paddingTop: '10px', paddingBottom: '10px' }}>
+//               <SelectField name="type" />
+//               <TextField name="name" />
+//               <TextField name="description" />
+//             </Form.Group>
+//             <SubmitField style={{
+//   display: 'block',
+//   marginLeft: 'auto', marginRight: 'auto', marginBottom: '10px',
+// }}/>
+//           </AutoForm>
+//           </Card>
+//         </Segment>
+//         </Container>
     );
   }
 }
