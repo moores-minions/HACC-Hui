b291d1a3d17cf0bd79b8d31a047cedd3d4eb0466
Jayden Tactay <75406323+jaydent22@users.noreply.github.com>
2023-11-08 12:54:03 -1000

    Conversion for EditTeamWidget.jsx

--- app/imports/ui/pages/participant/EditTeamPage.jsx
+++ app/imports/ui/pages/participant/EditTeamPage.jsx
@@ -1,14 +1,7 @@
 import React from 'react';
 
-import withAllSubscriptions from '../../layouts/AllSubscriptionsHOC';
 import EditTeamWidget from '../../components/participant/EditTeamWidget';
 
-class EditTeamPage extends React.Component {
-  render() {
-    return (
-        <EditTeamWidget />
-    );
-  }
-}
+const EditTeamPage = () => <EditTeamWidget />;
 
-export default withAllSubscriptions(EditTeamPage);
+export default EditTeamPage;
