bd4640c3012b96a745c93490c90604857ad6624b
Jayden Tactay <75406323+jaydent22@users.noreply.github.com>
2023-11-27 10:07:15 -1000

    Update EditProfilePage.jsx

--- app/imports/ui/pages/participant/EditProfilePage.jsx
+++ app/imports/ui/pages/participant/EditProfilePage.jsx
@@ -3,12 +3,6 @@ import React from 'react';
 import withAllSubscriptions from '../../layouts/AllSubscriptionsHOC';
 import EditProfileWidget from '../../components/participant/EditProfileWidget';
 
-class EditProfilePage extends React.Component {
-  render() {
-    return (
-        <EditProfileWidget />
-    );
-  }
-}
+const EditProfilePage = () => <EditProfileWidget />;
 
 export default withAllSubscriptions(EditProfilePage);
