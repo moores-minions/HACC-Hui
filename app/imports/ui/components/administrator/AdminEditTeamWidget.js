3174fff59522426237a5c656617cf1f3a853711c
Hooobot <hoobot0@gmail.com>
2023-11-29 01:40:35 -1000

    useTracker conversion fix for page load

--- app/imports/ui/components/administrator/AdminEditTeamWidget.js
+++ app/imports/ui/components/administrator/AdminEditTeamWidget.js
@@ -40,10 +40,7 @@ const AdminEditTeamWidget = (props) => {
    * @param formRef {FormRef} reference to the form.
    */
   const submit = (data) => {
-
-    const {
-      name, description, gitHubRepo, _id,
-    } = data;
+    const { name, description, gitHubRepo, _id } = data;
 
     const updateData = {
       id: _id,
@@ -53,83 +50,95 @@ const AdminEditTeamWidget = (props) => {
     };
 
     const collectionName = Teams.getCollectionName();
-    updateMethod.call({ collectionName, updateData },
-        (error) => {
-          if (error) {
-            swal('Error', error.message, 'error');
-          } else {
-            swal('Success', 'Item edited successfully', 'success');
-            setRedirect(true);
-
-          }
-        });
+    updateMethod.call({ collectionName, updateData }, (error) => {
+      if (error) {
+        swal('Error', error.message, 'error');
+      } else {
+        swal('Success', 'Item edited successfully', 'success');
+        setRedirect(true);
+      }
+    });
   };
 
   // Render the form. Use Uniforms: https://github.com/vazco/uniforms
 
-    const formSchema = new SimpleSchema2Bridge(schema);
-    const memberNamesAndGitHub = _.map(props.members, (p) => {
-      const fullName = Participants.getFullName(p._id);
-      const gitHub = p.gitHub;
-      return `${fullName}, (${gitHub})`;
-    });
+  const formSchema = new SimpleSchema2Bridge(schema);
+  const memberNamesAndGitHub = _.map(props.members, (p) => {
+    const fullName = Participants.getFullName(p._id);
+    const gitHub = p.gitHub;
+    return `${fullName}, (${gitHub})`;
+  });
   if (redirect) {
-    return <Redirect to={ROUTES.VIEW_TEAMS}/>;
+    return <Redirect to={ROUTES.VIEW_TEAMS} />;
   }
 
-    return (
-        <Container>
-            <div style={{
-              backgroundColor: '#E5F0FE', padding: '1rem 0rem', margin: '2rem 0rem',
-              borderRadius: '2rem',
-            }}>
-              <h2 className='text-center'>Edit Team</h2>
-            </div>
-            <AutoForm schema={formSchema} onSubmit={data => submit(data)} model={props.team}
-                      style={{
-                        paddingBottom: '4rem',
-                      }}>
-              <Card style={{
-                borderRadius: '1rem',
-                backgroundColor: '#E5F0FE',
-              }} className={'teamCreate'}>
-                  <Card.Body style={{ paddingLeft: '3rem', paddingRight: '3rem' }}>
-                    <TextField name='name' disabled />
-                    <LongTextField name='description' required/>
-                    <h4>Team Members:</h4>
-                    <ListGroup>
-                      {memberNamesAndGitHub.map((n) => <ListGroup.Item key={n}>{n}</ListGroup.Item>)}
-                    </ListGroup>
-                    <TextField name='gitHubRepo' required/>
-                  </Card.Body>
-                <div align='center'>
-                  <SubmitField value='Submit'
-                               style={{
-                                 color: 'white',
-                                 margin: '2rem 0rem',
-                               }}/>
-                </div>
-                <ErrorsField/>
-              </Card>
-            </AutoForm>
-        </Container>
-    );
+  return (
+    <Container>
+      <div
+        style={{
+          backgroundColor: '#E5F0FE',
+          padding: '1rem 0rem',
+          margin: '2rem 0rem',
+          borderRadius: '2rem',
+        }}
+      >
+        <h2 className="text-center">Edit Team</h2>
+      </div>
+      <AutoForm
+        schema={formSchema}
+        onSubmit={(data) => submit(data)}
+        model={props.team}
+        style={{
+          paddingBottom: '4rem',
+        }}
+      >
+        <Card
+          style={{
+            borderRadius: '1rem',
+            backgroundColor: '#E5F0FE',
+          }}
+          className={'teamCreate'}
+        >
+          <Card.Body style={{ paddingLeft: '3rem', paddingRight: '3rem' }}>
+            <TextField name="name" disabled />
+            <LongTextField name="description" required />
+            <h4>Team Members:</h4>
+            <ListGroup>
+              {memberNamesAndGitHub.map((n) => (
+                <ListGroup.Item key={n}>{n}</ListGroup.Item>
+              ))}
+            </ListGroup>
+            <TextField name="gitHubRepo" required />
+          </Card.Body>
+          <div style="text-align: center;">
+            <SubmitField
+              value="Submit"
+              style={{
+                color: 'white',
+                margin: '2rem 0rem',
+              }}
+            />
+          </div>
+          <ErrorsField />
+        </Card>
+      </AutoForm>
+    </Container>
+  );
 };
 
 AdminEditTeamWidget.propTypes = {
   team: PropTypes.object,
-  members: PropTypes.arrayOf(
-      PropTypes.object,
-  ).isRequired,
+  members: PropTypes.arrayOf(PropTypes.object).isRequired,
   model: PropTypes.object,
 };
 
-const AdminEditTeamCon = withTracker(({ match }) => {
+const AdminEditTeamCon = withRouter(({ match }) => {
   // Get the documentID from the URL field. See imports/ui/layouts/App.jsx for the route containing :_id.
   const documentId = match.params._id;
-  const team = Teams.findDoc(documentId);
-  const members = _.map(TeamParticipants.find({ teamID: team._id }).fetch(),
-      (tp) => Participants.findDoc(tp.participantID));
+  const team = useTracker(() => Teams.findDoc(documentId));
+  const members = useTracker(() => _.map(TeamParticipants.find(
+    { teamID: team._id },
+).fetch(), (tp) => Participants.findDoc(tp.participantID)));
   return {
     team,
     members,
