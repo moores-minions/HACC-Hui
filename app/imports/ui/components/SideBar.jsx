96e08cd950dc72063bc592199f63c35b5c096d6e
caciems <caciems@hawaii.edu>
2023-11-29 01:01:51 -1000

    SideBar conversion

--- app/imports/ui/components/SideBar.jsx
+++ app/imports/ui/components/SideBar.jsx
@@ -3,7 +3,8 @@ import PropTypes from 'prop-types';
 import { Meteor } from 'meteor/meteor';
 import { withTracker } from 'meteor/react-meteor-data';
 import { withRouter, NavLink } from 'react-router-dom';
-import { Menu, Header, Sidebar, Segment, Icon } from 'semantic-ui-react';
+import { Navbar, Offcanvas, NavDropdown, Nav, Container } from 'react-bootstrap';
+// import * as Icon from 'react-bootstrap-icons';
 import { Roles } from 'meteor/alanning:roles';
 import _ from 'lodash';
 import { ROLE } from '../../api/role/Role';
@@ -49,138 +50,133 @@ class SideBar extends React.Component {
 
     return (
         <div>
-          <Menu borderless inverted fixed={'top'} className={'mobileBar'}>
-            <Menu.Item position={'left'}>
-              <div onClick={() => setVisible(!this.state.visible)} style={{ padding: '5px' }}>
-                <Icon name='bars'/>
-              </div>
-            </Menu.Item>
-          </Menu>
-          <Sidebar.Pushable as={Segment} className={'sideBar'}>
-            <Sidebar
-                style={{ paddingTop: '4rem', backgroundColor: 'rgb(18, 72, 132)' }}
-                as={Menu}
-                animation='overlay'
-                icon='labeled'
-                inverted
-                vertical
-                onHide={() => setVisible(false)}
-                visible={this.state.visible}
-                width='thin'
+          <Navbar inverted fixed={'top'} className={'mobileBar'}>
+            <Navbar.Toggle aria-controls={'offcanvasNavbar-expand-false'}/>
+            <Navbar.Collapse id="navbar-offcanvas"></Navbar.Collapse>
+            <Navbar.Offcanvas
+              id="offcanvasNavbar-expand-false"
+              aria-labelledby={'offcanvasNavbarLabel-expand-false'}
             >
-              <Menu.Item as={NavLink} activeClassName="" exact to={ROUTES.LANDING}
+              <Offcanvas.Header closeButton/>
+            <Offcanvas.Body>
+            <div onClick={() => setVisible(!this.state.visible)} style={{ padding: '5px' }}>
+              <Nav className="justify-content-end flex-grow-1 pe-3">
+                <NavDropdown title="..." id="offcanvasNavbarDropdown-expand-false"
+                             >
+            <NavDropdown.Item as={NavLink} activeClassName="" exact to={ROUTES.LANDING}
                          onClick={() => setVisible(!this.state.visible)}>
-                <Header inverted as='h1'>HACC-Hui</Header>
-              </Menu.Item>
+                <h1>HACC-Hui</h1>
+              </NavDropdown.Item>
               {isParticipant ? (
                   [
-                      <Menu.Item as={NavLink}
+                      <NavDropdown.Item as={NavLink}
                                activeClassName="active"
                                disabled={!isCompliant}
                                exact
                                to={ROUTES.CREATE_TEAM}
-                               key='team-creation'>Create a Team</Menu.Item>,
-                    <Menu.Item as={NavLink}
+                               key='team-creation'>Create a Team</NavDropdown.Item>,
+                    <NavDropdown.Item as={NavLink}
                                activeClassName="active"
                                exact
                                to={ROUTES.YOUR_PROFILE}
-                               key='edit-profile'>Your Profile</Menu.Item>,
-                    <Menu.Item as={NavLink}
+                               key='edit-profile'>Your Profile</NavDropdown.Item>,
+                    <NavDropdown.Item as={NavLink}
                                activeClassName="active"
                                exact
                                to={ROUTES.BEST_FIT}
-                               key='list-teams'>List the Teams ({numTeams})</Menu.Item>,
-                    <Menu.Item as={NavLink}
+                               key='list-teams'>List the Teams ({numTeams})</NavDropdown.Item>,
+                    <NavDropdown.Item as={NavLink}
                                activeClassName="active"
                                disabled={!isCompliant}
                                exact
                                to={ROUTES.YOUR_TEAMS}
                                key='your-teams'>Your
-                      Teams</Menu.Item>,
-                    <Menu.Item as={NavLink}
+                      Teams</NavDropdown.Item>,
+                    <NavDropdown.Item as={NavLink}
                                activeClassName="active"
                                exact to={ROUTES.LIST_PARTICIPANTS}
-                               key='list-participants'>List the Participants ({numParticipants})</Menu.Item>,
-                    <Menu.Item as={NavLink}
+                               key='list-participants'>List the Participants ({numParticipants})</NavDropdown.Item>,
+                    <NavDropdown.Item as={NavLink}
                                activeClassName="active"
                                exact
                                to={ROUTES.SUGGEST_TOOL_SKILL}
-                               key='suggest-tool-skill'>Suggest Tool/Skill</Menu.Item>,
-                    <Menu.Item as={NavLink}
+                               key='suggest-tool-skill'>Suggest Tool/Skill</NavDropdown.Item>,
+                    <NavDropdown.Item as={NavLink}
                                activeClassName="active"
                                exact
                                to={ROUTES.TEAM_INVITATIONS}
-                               key='team-invitations'>Your Invitations</Menu.Item>,
+                               key='team-invitations'>Your Invitations</NavDropdown.Item>,
                   ]
               ) : ''}
               {isAdmin ? (
                   [
-                    <Menu.Item as={NavLink}
+                    <NavDropdown.Item as={NavLink}
                                activeClassName="active"
                                exact
                                to={ROUTES.CONFIGURE_HACC}
-                               key={ROUTES.CONFIGURE_HACC}>Configure HACC</Menu.Item>,
-                    <Menu.Item as={NavLink}
+                               key={ROUTES.CONFIGURE_HACC}>Configure HACC</NavDropdown.Item>,
+                    <NavDropdown.Item as={NavLink}
                                activeClassName="active"
                                exact
                                to={ROUTES.UPDATE_MP}
                                key={ROUTES.UPDATE_MP}>
                       Update Minor Participants Status ({uncompliantMinors})
-                    </Menu.Item>,
-                    <Menu.Item as={NavLink}
+                    </NavDropdown.Item>,
+                    <NavDropdown.Item as={NavLink}
                                activeClassName="active"
                                exact
                                to={ROUTES.LIST_SUGGESTIONS}
-                               key={ROUTES.LIST_SUGGESTIONS}>Suggestions List ({suggestionCount})</Menu.Item>,
-                    <Menu.Item as={NavLink}
+                               key={ROUTES.LIST_SUGGESTIONS}>Suggestions List ({suggestionCount})</NavDropdown.Item>,
+                    <NavDropdown.Item as={NavLink}
                                activeClassName="active"
                                exact
                                to={ROUTES.VIEW_TEAMS}
-                               key={ROUTES.VIEW_TEAMS}>View Team ({teamCount})</Menu.Item>,
-                    <Menu.Item as={NavLink}
+                               key={ROUTES.VIEW_TEAMS}>View Team ({teamCount})</NavDropdown.Item>,
+                    <NavDropdown.Item as={NavLink}
                                activeClassName="active"
                                exact
                                to={ROUTES.DUMP_DATABASE}
-                               key={ROUTES.DUMP_DATABASE}>Dump Database</Menu.Item>,
+                               key={ROUTES.DUMP_DATABASE}>Dump Database</NavDropdown.Item>,
                   ]
               ) : ''}
-              <Menu.Item>
+              <NavDropdown.Item>
                 {this.props.currentUser === '' ? (
-                    <Menu.Item as={NavLink} activeClassName="active" exact to={ROUTES.SIGN_IN}
+                    <NavDropdown.Item as={NavLink} activeClassName="active" exact to={ROUTES.SIGN_IN}
                                key={ROUTES.SIGN_IN}
-                               onClick={() => setVisible(!this.state.visible)}>Sign In</Menu.Item>
+                               onClick={() => setVisible(!this.state.visible)}>Sign In</NavDropdown.Item>
                 ) : (
-                    [<Menu.Item as={NavLink} activeClassName="active" exact to={ROUTES.SIGN_OUT}
+                    [<NavDropdown.Item as={NavLink} activeClassName="active" exact to={ROUTES.SIGN_OUT}
                                key={ROUTES.SIGN_OUT}
-                               onClick={() => setVisible(!this.state.visible)}>Sign Out</Menu.Item>,
-                      <Menu.Item as={NavLink} activeClassName="active" exact to={ROUTES.DELETE_ACCOUNT}
+                               onClick={() => setVisible(!this.state.visible)}>Sign Out</NavDropdown.Item>,
+                      <NavDropdown.Item as={NavLink} activeClassName="active" exact to={ROUTES.DELETE_ACCOUNT}
                                  key={ROUTES.DELETE_ACCOUNT}
-                                 onClick={() => setVisible(!this.state.visible)}>Delete Account</Menu.Item>,
+                                 onClick={() => setVisible(!this.state.visible)}>Delete Account</NavDropdown.Item>,
                     ]
                 )}
-              </Menu.Item>
-            </Sidebar>
-            <Sidebar.Pusher style={{ paddingTop: '5rem' }}>
+              </NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
+            </div>
+            </Offcanvas.Body>
+            </Navbar.Offcanvas>
+          </Navbar>
+            <Container style={{ paddingTop: '5rem' }} fluid="md">
               {this.props.children}
-            </Sidebar.Pusher>
-          </Sidebar.Pushable>
+            </Container>
         </div>
 
     );
   }
 }
 
-// Declare the types of all properties.
 SideBar.propTypes = {
   currentUser: PropTypes.string,
   children: PropTypes.array,
   visible: PropTypes.bool,
 };
 
-// withTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker
 const SideBarContainer = withTracker(() => ({
   currentUser: Meteor.user() ? Meteor.user().username : '',
 }))(SideBar);
 
-// Enable ReactRouter for this component. https://reacttraining.com/react-router/web/api/withRouter
 export default withRouter(SideBarContainer);
